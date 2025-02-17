d3332a0baf8e42030e5343be8ff4496c92b745d0
czarekk215 <czarekmadziewczyne@gmail.com>
2025-02-17 10:22:55 +0100

    Changed licenses headers. Removed license_header.py. Changed branch in repos file to main.

--- install_depends.sh
+++ install_depends.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
-# 
-#  Copyright (C) 2025  wisevision
-# 
+#!/bin/sh
+#
+#  Copyright (C) 2025 wisevision
+#
+#  SPDX-License-Identifier: MPL-2.0
+#
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-
 
 export DEBIAN_FRONTEND=noninteractive
 export GRPC_INSTALL_DIR=$HOME/grpc_install_dir
